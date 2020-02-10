169eebb692d613616d0d0b2ac86499597d3e37b2
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:07:55 -0800

    Added IHumanOutput extensions.

---
+++ source/R5T.Bath.Base/Code/Services/Extensions/IHumanOutputExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Bath
+{
+    public static class IHumanOutputExtensions
+    {
+        public static void WriteBlankLine(this IHumanOutput humanOutput)
+        {
+            humanOutput.WriteLine(String.Empty);
+        }
+    }
+}
